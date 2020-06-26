9f7de5cf5868a5ceb14b7bc81e882694eecc1917
ashish saklani <ashishsaklani03@gmail.com>
2020-06-26 22:42:22 +0530

    screen Size Program.

--- PractWin32/PractWin32/Source.cpp
+++ PractWin32/PractWin32/Source.cpp
@@ -1,9 +1,28 @@
 #include<Windows.h>
+#include<tchar.h>
+#include<stdio.h>
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
+int _cdecl MessageBoxPrintf(const TCHAR* szCaption, const TCHAR* szFormat, ...)
 {
+	TCHAR szBuffer[1024];
+	va_list pArgList;
 
-	MessageBox(NULL, TEXT("Hello World"), TEXT("Gello msg"), 0);
+	va_start(pArgList, szFormat);
 
+	_vstprintf(szBuffer, sizeof(szBuffer) / sizeof(TCHAR), szFormat, pArgList);
+
+	va_end(pArgList);
+
+	return MessageBox(NULL, szBuffer, szCaption, 0);
+
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR zcCmdLine, int iCmdShow)
+{
+	int cxScreen, cyScreen;
+	cxScreen = GetSystemMetrics(SM_CXSCREEN);
+	cyScreen = GetSystemMetrics(SM_CYSCREEN);
+
+	MessageBoxPrintf(TEXT("ScrnSize"), TEXT("the Screen size is %i wide by %i pixels high."), cxScreen, cyScreen);
 	return 0;
 }

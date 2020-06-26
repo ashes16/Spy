682030674c68f2f7a2ae674ee367f759833bae8f
ashish saklani <ashishsaklani03@gmail.com>
2020-06-26 22:27:54 +0530

    Hello World in Win32.

---
+++ PractWin32/PractWin32/Source.cpp
@@ -0,0 +1,9 @@
+#include<Windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
+{
+
+	MessageBox(NULL, TEXT("Hello World"), TEXT("Gello msg"), 0);
+
+	return 0;
+}

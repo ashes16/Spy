06066d4437a9bb725c4791a944791006140fb6d8
ashish saklani <ashishsaklani03@gmail.com>
2020-07-14 23:55:20 +0530

    changing the color of text by taking the inputs of keys like g for green and so on..

--- PractWin32/PractWin32/Source.cpp
+++ PractWin32/PractWin32/Source.cpp
@@ -1,6 +1,7 @@
 #include<Windows.h>
 #include"SystemMetrics.h"
 
+int ipaintflag=0;
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
@@ -47,11 +48,70 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT imsg, WPARAM wparam, LPARAM lparam)
 	PAINTSTRUCT ps;
 	switch (imsg)
 	{
+	case WM_CHAR:
+		switch (wparam)
+		{
+		case 'r':
+		case 'R':
+			ipaintflag = 1;
+			InvalidateRect(hwnd, NULL, TRUE);
+			break;
+		case 'g':
+		case 'G':
+			ipaintflag = 2;
+			InvalidateRect(hwnd, NULL, TRUE);
+			break;
+		case 'b':
+		case'B':
+			ipaintflag = 3;
+			InvalidateRect(hwnd, NULL, TRUE);
+			break;
+		case 'c':
+		case'C':
+			ipaintflag = 4;
+			InvalidateRect(hwnd, NULL, TRUE);
+			break;
+		case'm':
+		case 'M':
+			ipaintflag = 5;
+			InvalidateRect(hwnd, NULL, TRUE);
+			break;
+		case 'y':
+		case'Y':
+			ipaintflag = 6;
+			InvalidateRect(hwnd, NULL, TRUE);
+			break;
+		}
+		break;
+	case WM_LBUTTONDOWN:
+		ipaintflag = 7;
+		InvalidateRect(hwnd, NULL, TRUE);
+		break;
+	case WM_RBUTTONDOWN:
+		ipaintflag = 8;
+		InvalidateRect(hwnd, NULL, TRUE);
+		break;
 	case WM_PAINT:
 		GetClientRect(hwnd, &rect);
 		hdc = BeginPaint(hwnd, &ps);
 		SetBkColor(hdc, RGB(0, 0, 0));
-		SetTextColor(hdc, RGB(0, 255, 0));
+		if (ipaintflag == 1)
+			SetTextColor(hdc, RGB(255, 0, 0));
+		else if (ipaintflag == 2)
+			SetTextColor(hdc, RGB(0, 255, 0));
+		else if (ipaintflag == 3)
+			SetTextColor(hdc, RGB(0, 0, 255));
+		else if (ipaintflag == 4)
+			SetTextColor(hdc, RGB(0, 255, 255));
+		else if (ipaintflag == 5)
+			SetTextColor(hdc, RGB(255, 0, 255));
+		else if (ipaintflag == 6)
+			SetTextColor(hdc, RGB(255, 225, 0));
+		else if (ipaintflag == 7)
+			SetTextColor(hdc, RGB(255, 255, 255));
+		else if (ipaintflag == 8)
+			SetTextColor(hdc, RGB(255, 165, 0));
+
 		DrawText(hdc, str, -1, &rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
 		EndPaint(hwnd, &ps);
 		break;
